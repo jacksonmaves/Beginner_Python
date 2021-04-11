e4d5ca5988b15c01e1143345bb85265f9d4b6bc6
jacksonmaves <82250489+jacksonmaves@users.noreply.github.com>
2021-04-10 21:00:00 -0400

    Adding two files for basic computation and short experiments with if functions

---
+++ Computation.py
@@ -0,0 +1,11 @@
+import random
+x=2.6+2+6.6+5.5+2+0.6+1+2+2.04+2+3.2+1.96+2+2.9+3+4.25+6.53
+print(x)
+r = random.randint(1,10)
+if r > 3:
+    print(r, "is greater than 3")
+if r < 3:
+        print(r, "is smaller than 3")
+if r == 3:
+    print("That's 3")
+print(r)
